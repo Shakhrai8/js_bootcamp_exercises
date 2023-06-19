d7e5c49935e11a52627dd12491a845de1e152cb9
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-06-19 17:48:51 +0100

    tdd new methods, pass tests

---
+++ notes-app/notes.js
@@ -0,0 +1,20 @@
+class NotesModel {
+
+  constructor() {
+    this.array = [];
+  };
+
+  addNote(string) {
+    return this.array.push(string);
+  };
+
+  getNotes() {
+    return this.array;
+  };
+
+  reset() {
+    return this.array = [];
+  };
+};
+
+module.exports = NotesModel;
---
+++ notes-app/notes.test.js
@@ -0,0 +1,34 @@
+const NotesModel = require("./notes");
+
+describe("NotesModel", () => {
+  let notes;
+
+  beforeEach(() => {
+    notes = new NotesModel();
+  });
+
+  it("returns an empty array", () => {
+    expect(notes.getNotes()).toEqual([]);
+  });
+
+  it("returns 2 notes in the array", () => {
+    let note_1 = "Test note";
+    let note_2 = "Another test note";
+    
+    notes.addNote(note_1);
+    notes.addNote(note_2);
+    expect(notes.getNotes()).toEqual([note_1, note_2]);
+  });
+
+  it("resets the array and returns 1 note", () => {
+    let note_1 = "Test note";
+    let note_2 = "Another test note";
+    let note_3 = "Something else";
+    
+    notes.addNote(note_1);
+    notes.addNote(note_2);
+    notes.reset();
+    notes.addNote(note_3);
+    expect(notes.getNotes()).toEqual([note_3]);
+  });
+})
