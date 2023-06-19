0d641483082c0cbed708cf14bc1593edb7453ddc
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-06-19 20:32:36 +0100

    tdd add and clear methods, pass tests

--- example-2/view.js
+++ example-2/view.js
@@ -4,6 +4,17 @@ class View {
 
     console.log(this.mainContainerEl);
   }
+
+  addParagraph() {
+    const newPara = document.createElement("p");
+    newPara.textContent = "This paragraph has been dynamically added by JavaScript!";
+    document.querySelector("#main-container").append(newPara);
+  }
+
+  clearParagraphs() {
+    const par = document.querySelectorAll("p");
+    par.forEach((p) => p.remove());
+  }
 }
 
 module.exports = View;
