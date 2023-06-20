20698c30ae5b73ccce42468e69fc3f0122c29c33
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-06-20 21:20:23 +0100

    refactor

--- notes-app/index.js
+++ notes-app/index.js
@@ -1,9 +1,9 @@
 const NotesModel = require("./notes.js");
 const NotesView = require("./notes_view.js");
-const NotesClient = require("./notesClient.js");
+const NoteClient = require("./noteClient.js");
 
 const notes = new NotesModel();
-notes.addNote("Note from the server");
-const client = new NotesClient();
+const client = new NoteClient();
 const view = new NotesView(notes, client);
 view.displayNotes();
+view.displayNotesFromApi();
