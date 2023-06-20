744c29a1d2708328eef06ca838ae76232455decf
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-06-20 13:07:36 +0100

    add the display method, works

--- example-4/githubView.js
+++ example-4/githubView.js
@@ -3,20 +3,28 @@ class GithubView {
     this.model = model;
     this.client = client;
 
-    const submitButtonEl = document.querySelector('#submit-button');
-    const repoInputEl = document.querySelector('#repo-name-input');
+    const submitButtonEl = document.querySelector("#submit-button");
+    const repoInputEl = document.querySelector("#repo-name-input");
 
-    submitButtonEl.addEventListener('click', () => {
+    submitButtonEl.addEventListener("click", () => {
       const repoName = repoInputEl.value;
 
-      this.client.getRepoInfo(repoName, repoData => {
+      this.client.getRepoInfo(repoName, (repoData) => {
         console.log(repoData);
+        this.display(repoData);
       });
     });
   }
 
-  display() {
+  display(repoData) {
+    const name = document.querySelector("#repo-name");
+    name.textContent = repoData.full_name;
 
+    const description = document.querySelector("#repo-description");
+    description.textContent = repoData.description;
+
+    const image = document.querySelector("#repo-image");
+    image.src = repoData.organization.avatar_url;
   }
 }
 
