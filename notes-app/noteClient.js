c4f6ade3450eff57bc7bc7e34de9a4920e263d41
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-06-21 21:09:55 +0100

    supports emoji input, not tested

--- notes-app/noteClient.js
+++ notes-app/noteClient.js
@@ -6,13 +6,35 @@ class noteClient {
       .catch((error) => error_msg(error));
   }
 
-  createNote(data, error_msg) {
+  async createNote(data, error_msg) {
+    const convertEmojiShortcodes = async (content) => {
+      const response = await fetch("https://makers-emojify.herokuapp.com/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ text: content }),
+      });
+
+      const { status, emojified_text, message } = await response.json();
+
+      if (status === "OK") {
+        return emojified_text;
+      } else {
+        throw new Error(message);
+      }
+    };
+
+    const convertedData = {
+      content: await convertEmojiShortcodes(data.content),
+    };
+
     return fetch("http://localhost:3000/notes", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(data),
+      body: JSON.stringify(convertedData),
     }).then((response) => response.json());
   }
 }
